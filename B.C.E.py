1c7603b6d125d99ecb5d7181874d81b1731ac89d
Gourav_patel <gourav@python.cwh>
2022-02-15 22:49:35 +0530

    bce

--- B.C.E.py
+++ B.C.E.py
@@ -39,7 +39,7 @@ print(set_list)
 
 # ------------ MAKING TABLE LIST -------------------------------------------------
 table_list = []
-st = [ "A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
+st = [ "A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","AA","BB","CC","DD","EE","FF","II"]
 st_num = 0
 temp = 0
 temp_value = None
