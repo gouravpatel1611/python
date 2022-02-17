7fbbd900a4b73957444866e7549f3934d2c156c3
Gourav_patel <gourav@python.cwh>
2022-02-18 02:59:44 +0530

    yaha par maine usi program ko c langwage me likha hai

---
+++ Programming Problems/mission_iit.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+
+int main()
+{
+
+    int user_input;
+    printf("Enter your number :- ");
+    scanf("%d", &user_input);
+
+    int arr[user_input + 1];
+
+    int temp = 1;
+
+    while (1)
+    {
+        int t = 0;
+        for (int i = 0; i < user_input - temp; i++)
+        {
+            printf("1+");
+            arr[t] = 1;
+            t++;
+        }
+        printf("%d", temp);
+        arr[t] = temp;
+        printf("\n");
+
+        if (temp == user_input)
+        {
+            break;
+        }
+        int t2 = 1;
+        while(arr[t] - arr[t - t2] > 1)
+        {
+            arr[t]--;
+            arr[t - t2]++;
+
+            for (int i = 0; i <= t - 1; i++)
+            {
+                printf("%d+",arr[i]);
+            }
+            printf("%d", arr[t]);
+            printf("\n");
+            t2++;
+        }
+        temp++;
+    }
+
+    return 0;
+}
